5012da3895a0df7a6a01b2f9d63d753c46c171dc
JamesRymarc29 <luffytarrojames@gmail.com>
2024-03-12 08:19:19 +0800

    Added button alert and action sheet

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle 
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
+  IonAlert, IonButton, IonActionSheet
+  
 } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
@@ -23,13 +25,54 @@ const Tab1: React.FC = () => {
         <IonCard>
       <img alt="Silhouette of mountains" src="../src/assets/img/avatar.png" />
       <IonCardHeader>
-        <IonCardTitle>Card Title</IonCardTitle>
-        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        <IonCardTitle>My Profile</IonCardTitle>
+        <IonCardSubtitle>Good Student</IonCardSubtitle>
       </IonCardHeader>
 
-      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+      <IonCardContent>Don’t try to be perfect. Just try to be better than you were yesterday</IonCardContent>
+      <IonButton id="present-alert">Click Me</IonButton>
+      <IonAlert
+        trigger="present-alert"
+        header="A Short Title Is Best"
+        subHeader="A Sub Header Is Optional"
+        message="A message should be a short, complete sentence."
+        buttons={['Action']}
+      ></IonAlert>  
+
+    <IonButton id="open-action-sheet">Open</IonButton>
+      <IonActionSheet
+        trigger="open-action-sheet"
+        header="Actions"
+        buttons={[
+          {
+            text: 'Delete',
+            role: 'destructive',
+            data: {
+              action: 'delete',
+            },
+          },
+          {
+            text: 'Share',
+            data: {
+              action: 'share',
+            },
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            data: {
+              action: 'cancel',
+            },
+          },
+        ]}
+      ></IonActionSheet>
+
+      
+
     </IonCard>
+    
       </IonContent>
+      
     </IonPage>
   );
 };
