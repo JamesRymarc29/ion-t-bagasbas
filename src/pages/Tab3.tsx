a46993c461ab5d463a39f8473a2a1d4fe4020efc
JamesRymarc29 <luffytarrojames@gmail.com>
2024-04-02 08:35:18 +0800

    Added Click counter

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,28 +1,39 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import './Tab2.css';
 
-const Tab3: React.FC = () => {
+const Tab2: React.FC = () => {
+  // Step 2: Create state variable for click count
+  const [clickCount, setClickCount] = useState(0);
+
+  // Step 3: Define function to increment click count
+  const incrementCount = () => {
+    setClickCount(prevCount => prevCount + 1);
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Click Counter</IonTitle>
           </IonToolbar>
         </IonHeader>
 
-       
-
-        <ExploreContainer name="Tab 3 page" />
+        <div className="ion-padding">
+          {/* Step 4: Display click counter */}
+          <p>Click Count: {clickCount}</p>
+          {/* Step 4: Add a button to increment click count */}
+          <IonButton onClick={incrementCount}> Please Click!</IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab3;
+export default Tab2;
