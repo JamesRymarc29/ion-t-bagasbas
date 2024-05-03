b59fd7265cbe44c7dd8b3ae4c6e6b31accfa6ac3
JamesRymarc29 <luffytarrojames@gmail.com>
2024-05-03 18:48:05 +0800

    added back button in the profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
-  IonAlert, IonButton, IonActionSheet
-  
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
+  IonAlert, IonButton, IonActionSheet, IonButtons, IonBackButton
 } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Profile.css';
 
 const Profile: React.FC = () => {
@@ -11,69 +9,63 @@ const Profile: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>profile</IonTitle>
+          {/* Back Button */}
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/" />
+          </IonButtons>
+          {/* Title */}
+          <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
 
-      
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Profile</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <IonCard>
-      <img alt="Silhouette of mountains" src="../src/assets/img/avatar.png" />
-      <IonCardHeader>
-        <IonCardTitle>My Profile</IonCardTitle>
-        <IonCardSubtitle>Good Student</IonCardSubtitle>
-      </IonCardHeader>
+          <img alt="Silhouette of mountains" src="../src/assets/img/avatar.png" />
+          <IonCardHeader>
+            <IonCardTitle>My Profile</IonCardTitle>
+            <IonCardSubtitle>Good Student</IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>Don’t try to be perfect. Just try to be better than you were yesterday</IonCardContent>
+          {/* About Me Button */}
+          <IonButton id="present-alert" color="success" expand='full'>About Me</IonButton>
+          <IonAlert
+            trigger="present-alert"
+            header="James Rymarc Bagasbas"
+            subHeader="BSIT student"
+            message="If you don't take risks, you can't create a future."
+            buttons={['Back']}
+          ></IonAlert>
 
-      <IonCardContent>Don’t try to be perfect. Just try to be better than you were yesterday</IonCardContent>
-      <IonButton id="present-alert" color="success" expand='full'>About Me</IonButton > 
-      <IonAlert
-        trigger="present-alert"
-        header="James Rymarc Bagasbas"
-        subHeader="BSIT student"
-        message="If you don't take risks, you can't create a future."
-        buttons={['Back']}
-        
-      ></IonAlert>  
-
-    <IonButton id="open-action-sheet" color="success" expand='full'>Open</IonButton>
-      <IonActionSheet
-        trigger="open-action-sheet"
-        header="Actions"
-        buttons={[ 
-          {
-            text: 'Delete',
-            role: 'destructive',
-            data: {
-              action: 'delete',
-            },
-          },
-          {
-            text: 'Share',
-            data: {
-              action: 'share',
-            },
-          },
-          {
-            text: 'Cancel',
-            role: 'cancel',
-            data: {
-              action: 'cancel',
-            },
-          },
-        ]}
-      ></IonActionSheet>
-
-      
-
-    </IonCard>
-    
+          {/* Open Action Sheet Button */}
+          <IonButton id="open-action-sheet" color="success" expand='full'>Open</IonButton>
+          <IonActionSheet
+            trigger="open-action-sheet"
+            header="Actions"
+            buttons={[
+              {
+                text: 'Delete',
+                role: 'destructive',
+                data: {
+                  action: 'delete',
+                },
+              },
+              {
+                text: 'Share',
+                data: {
+                  action: 'share',
+                },
+              },
+              {
+                text: 'Cancel',
+                role: 'cancel',
+                data: {
+                  action: 'cancel',
+                },
+              },
+            ]}
+          ></IonActionSheet>
+        </IonCard>
       </IonContent>
-      
     </IonPage>
   );
 };
